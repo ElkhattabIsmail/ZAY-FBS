ed9f171cb5c3f676ed1c1e7715777d6cfbb93324
ElkhattabIsmail <elhattab207@gmail.com>
2026-04-20 15:48:09 +0100

    Ajouter la liste initial de Produits.

---
+++ script.js
@@ -0,0 +1,9 @@
+let products = [
+  { id: 1, name: "Huile d'Olive Bio", price: 80, desc: "Première pression à froid, acidité < 0,3%. Goût fruité et délicat.", 
+    img: "https://images.unsplash.com/photo-1474979266404-7eaacbcd87c5?w=400&q=80" },
+  { id: 2, name: "Huile d'Olive Bio", price: 80, desc: "Première pression à froid, acidité < 0,3%. Goût fruité et délicat.", 
+    img: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=400&q=80" },
+  { id: 3, name: "Huile d'Olive Bio", price: 80, desc: "Première pression à froid, acidité < 0,3%. Goût fruité et délicat.", 
+    img: "https://images.unsplash.com/photo-1601379327928-bedfaf9da2d0?w=400&q=80" },
+];
+
