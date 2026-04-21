28af36cef669ee843579ca73b0f8b7801b0409e6
ElkhattabIsmail <elhattab207@gmail.com>
2026-04-21 10:42:31 +0100

    showToast fonction.

--- script.js
+++ script.js
@@ -123,6 +123,17 @@ function closeModal() {
 
 
 
+function showToast(msg) {
+  const t = document.getElementById('toast');
+  t.textContent = msg;
+  t.classList.add('show');
+  setTimeout(() => t.classList.remove('show'), 2500);
+}
+
+
+
+
+
 
 
 
